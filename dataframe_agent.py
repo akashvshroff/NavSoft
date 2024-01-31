9b22b2662b2dfa24f339a1eed306fc1a6866ca36
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2024-01-31 14:20:05 -0600

    basic class template for agent

--- dataframe_agent.py
+++ dataframe_agent.py
@@ -1,44 +1,57 @@
-import os
+import os, logging
 import pandas as pd
 import secret
 
 # langchain imports
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
-from langchain_openai import ChatOpenAI, OpenAI
+from langchain_openai import ChatOpenAI
 
 os.environ["OPENAI_API_KEY"] = secret.OPENAI_KEY
+logging.basicConfig(
+    filename="df_agent_errors.txt", encoding="utf-8", level=logging.warning
+)
 
 
-def read_data(is_parquet, filename="./data/BaseTable_v1.parquet"):
-    if is_parquet:
-        return pd.read_parquet(filename)
-    else:
-        return pd.read_csv(filename)
+class DataframeAnalysisAgent:
+    def __init__(self, df, gpt4=True):
+        self.df = df
+        self.model = "gpt-4-0125-preview" if gpt4 else "gpt-3.5-turbo-1106"
+        self.agent = self.create_agent(0.1)
 
+    def create_agent(self, temp=0.1):
+        """
+        Create chat agent with given df and model.
+        """
+        agent = create_pandas_dataframe_agent(
+            ChatOpenAI(temperature=temp, model=self.model),
+            self.df,
+            verbose=False,  # set to true if debugging
+            agent_type=AgentType.OPENAI_FUNCTIONS,
+        )
+        return agent
 
-def create_agent(df, model="gpt-3.5-turbo-0613", temp=0):
-    agent = create_pandas_dataframe_agent(
-        ChatOpenAI(temperature=temp, model=model),
-        df,
-        verbose=True,
-        agent_type=AgentType.OPENAI_FUNCTIONS,
-    )
-    return agent
+    def load_new_df(self, df):
+        """
+        Function to add new dataframe and update agent
+        """
+        self.df = df
+        self.agent = self.create_agent(0.1)
 
+    def query(self, user_prompt):
+        """
+        Runs the query against the agent and returns response (or appropriate error)
+        """
+        prompt = f"""You are a helpful data analyst that will solve the provided user request using the dataframe appropriately.
+        It is important that you answer accurately. If you do not understand the question, or cannot answer it, be clear and ask for clarifications.
+        Remember that you should only answer questions about the dataframe. If a user request pertains to something apart from the dataframe, reply saying that 
+        the query is out of your domain.
 
-def driver():
-    df = read_data(True)
-    agent = create_agent(df)
-
-    while True:
-        prompt = input("Please enter your query below? \n")
+        User Request: {user_prompt}
+        """
         try:
-            agent.run(prompt)
+            response = self.agent.invoke(prompt)
+            return response["output"]
         except Exception as e:
-            print(f"There was an error: {e}. Please try again.")
-        print("-" * 25)
-
-
-if __name__ == "__main__":
-    driver()
+            logging.error(str(e))
+            return "An unknown error occured. Please try again later."
