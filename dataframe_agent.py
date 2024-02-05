d3bdb513c491239936a1fff91ddbe8046b535428
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2024-02-05 13:24:17 -0600

    added JSON output to analysis agent

--- dataframe_agent.py
+++ dataframe_agent.py
@@ -1,16 +1,31 @@
 import os, logging
 import pandas as pd
 import secret
+import json
+from typing import List
 
 # langchain imports
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
+
 
 os.environ["OPENAI_API_KEY"] = secret.OPENAI_KEY
 logging.basicConfig(
     filename="df_agent_errors.txt", encoding="utf-8", level=logging.WARNING
 )
+
+
+class Analysis(BaseModel):
+    status: int = Field(
+        description="status code for query, 0 for success, 1 for clarification, 2 for out of domain questions."
+    )
+    response: str = Field(
+        description="string output for query, either analysis, clarification question or error message"
+    )
 
 
 class DataframeAnalysisAgent(object):
@@ -29,6 +44,7 @@ class DataframeAnalysisAgent(object):
         self.df = df
         self.model = "gpt-4-0125-preview" if gpt4 else "gpt-3.5-turbo-1106"
         self.agent = self.create_agent(0.1)
+        self.parser = JsonOutputParser(pydantic_object=Analysis)
 
     def create_agent(self, temp=0.1):
         """
@@ -53,16 +69,32 @@ class DataframeAnalysisAgent(object):
         """
         Runs the query against the agent and returns response (or appropriate error)
         """
-        prompt = f"""You are a helpful data analyst that will solve the provided user request using the dataframe appropriately.
-        It is important that you answer accurately. If you do not understand the question, or cannot answer it, be clear and ask for clarifications.
-        Remember that you should only answer questions about the dataframe. If a user request pertains to something apart from the dataframe, reply saying that 
-        the query is out of your domain.
 
-        User Request: {user_prompt}
-        """
+        prompt_template = PromptTemplate(
+            template="""You are a helpful data analyst that will solve the provided user request using the dataframe appropriately.
+
+            It is important that you answer accurately. If you do not understand the question, or cannot answer it, be clear and ask for clarifications. Remember that you should only answer questions about the dataframe. 
+            
+            If a user request pertains to something apart from the dataframe, reply saying that the query is out of your domain. \n{format_instructions}\n{query}
+            
+            In all cases, make sure you adhere to the given output format instructions. If you need more information from users, return status = 1. If the question is out of domain, return status = 2.""",
+            input_variables=["query"],
+            partial_variables={
+                "format_instructions": self.parser.get_format_instructions()
+            },
+        )
+
         try:
+            prompt = prompt_template.format(query=user_prompt)
             response = self.agent.invoke(prompt)
-            return response["output"]
+            raw_obj = response["output"]
+            response_obj = self.parser.parse(raw_obj)
+            assert isinstance(response_obj, dict)
+            return response_obj
+
         except Exception as e:
             logging.error(str(e))
-            return "An unknown error occured. Please try again later."
+            return {
+                "status": 2,
+                "response": "An unknown error occurred. Please try again later.",
+            }
