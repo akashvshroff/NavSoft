50865a6db49f46a6b9f63b3e1d7f2a20a177cb48
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2024-02-05 14:24:16 -0600

    updated readme

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - To run the code, you must install the requirements, which can be done using `pip install -r requirements.txt`. 
 - Moreover, ensure that you have the data file saved locally and edit the path used to call it. 
 - Finally, you need API access to the OpenAI system. Using this, generate a key and add it to a file called secret.py (or set it as an environment variable and import it).
+- Read through the code in `example.py` to see how the agent can be used effectively.
--- example.py
+++ example.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 from dataframe_agent import DataframeAnalysisAgent
 
+from interface_agent import InterfaceAgent
+
 
 def read_data(is_parquet, filename="./data/BaseTable_v1.parquet"):
     if is_parquet:
@@ -11,18 +13,18 @@ def read_data(is_parquet, filename="./data/BaseTable_v1.parquet"):
 
 
 def driver():
-    df = read_data(True)
+    # df = read_data(True)
     # df needs to be read in client side and then passed in to agent
     # create analysis agent using dataframe - singleton class so only instantiates once, to change df use load_new_df
     # TODO: might need to keep loading new dfs every time the forecast is regenerated - but probably a good idea to avoid agent creation time
-    agent = DataframeAnalysisAgent(df, False)
+    agent = InterfaceAgent()
 
     while (
         True
     ):  # could be a running loop for queries with loading spinner - error handling done in agent itself
         prompt = input("Please enter your query below? \n\n")
         response = agent.query(prompt)
-        print(f"> {response}\n")
+        print(f"> {response['response']}\n")
         print("-" * 25)
 
 
